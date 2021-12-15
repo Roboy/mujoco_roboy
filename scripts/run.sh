2b35634d97fa2fab053b02080a7d899e913022bb
Tai Hoang <thobotics@gmail.com>
2021-12-15 14:29:19 +0100

    fix conflict

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,11 +4,7 @@ BRANCH=$(<branch.txt)
 docker build -t mujoco_tendon_$BRANCH .
 
 export DISPLAY=$DISPLAY;
-<<<<<<< HEAD:setup.sh
-export MUJOCO_PROJECT_PATH=~/Roboy/mujoco_tendon_$BRANCH/mujoco_tendon;
-=======
 export MUJOCO_PROJECT_PATH=<YOUR_PROJECT_PATH>;
->>>>>>> master:scripts/run.sh
 
 docker run \
     --env="DISPLAY=${DISPLAY}" \
@@ -19,10 +15,5 @@ docker run \
     -d \
     --network host \
     --privileged \
-<<<<<<< HEAD:setup.sh
-    --name mujoco_tendon_$BRANCH \
-    -it mujoco_tendon_$BRANCH /bin/bash
-=======
     --name ros-mujoco-tendon \
     -it ros-mujoco-tendon /bin/bash
->>>>>>> master:scripts/run.sh
