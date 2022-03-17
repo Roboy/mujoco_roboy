c01347579db81960c3d0d7098c14315067c257b0
Infraviored <floo.schneider@gmx.de>
2022-03-17 01:25:14 +0100

    PID tuning almost done

---
+++ eval_data/PID Analysis/findPID.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+
+parser = argparse.ArgumentParser(
+    description='Calculate PID values from Ultimate Gain Ku and Oscillation Periods.')
+parser.add_argument('Ku', type=float,
+                    help='Ultimate Gain, P value at which Ocscillations are self sustaining.')
+parser.add_argument('N', type=float,
+                    help='Number of oscillations (float) counted in the time window T')
+parser.add_argument('T', type=float,
+                    help='Time window [s] in which the oscillations have been counted')
+
+args = parser.parse_args()
+
+Ku = args.Ku
+Tu = args.T/args.N
+Kp = 0.6*Ku
+Ki = 1.2*Ku/Tu
+Kd = 0.075*Ku*Tu
+
+print('Ku = %.4f' % Ku)
+print('Tu = %.4f' % Tu)
+print('GAINS:')
+print('Kp = %.4f' % Kp)
+print('Ki = %.4f' % Ki)
+print('Kd = %.4f' % Kd)
--- src/runForParameters.sh
+++ src/runForParameters.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # run two processes in the background and wait for them to finish
 
-python3 simulation.py 24000 0 0 400 --log 10 --plot --bag shoulder_axis0_left_elbow_0_G 8 0 --ctrlOnly shoulder_left_axis0 --folder TuneShoulderP_axis0 --model model_nosidesite.xml --render 60
+python3 simulation.py 25500 0 0 400 --log 10 --plot --bag shoulder_axis0_left_elbow_0_G 8 0 --ctrlOnly shoulder_left_axis0 --folder TuneShoulderP_axis0 --model model_nosidesite.xml
 wait
 sleep 3
+
 
 
 
